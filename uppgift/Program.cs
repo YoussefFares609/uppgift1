7aaed9ed8d3e605803c786a0cc4c8448ef5d0f1b
YoussefFares609 <14YF0608@amal.se>
2025-09-15 11:00:58 +0200

    Add project files.

---
+++ uppgift/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Reflection;
+
+Random rand = new Random();
+
+Console.Write("Enter a number (0-36): ");
+
+
+
+while (true)
+{
+
+int winning = rand.Next(0, 37);
+int guess = Convert.ToInt32(Console.ReadLine());
+    if (guess < 0)
+    {
+        Console.WriteLine("I SAID 0-36!");
+    }
+    else if (guess > 36)
+        Console.WriteLine("I SAID 0-36!");
+
+if (guess == winning)
+    {
+        Console.WriteLine("You Won!");
+    }
+    else
+    {
+        Console.WriteLine("You Lost, try again");
+    }
+    
+}
+
+
+
